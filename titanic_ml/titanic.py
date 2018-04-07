2d34f60dca0b3381a0ae215168edba8b61ff5389
Phil <philhteoma@gmail.com>
2018-04-07 21:48:49 +0100

    Making code more readable

--- titanic_ml/titanic.py
+++ titanic_ml/titanic.py
@@ -4,12 +4,7 @@ import pandas as pd
 import sklearn as sk
 import numpy as np
 
-from sklearn.model_selection import cross_val_score
-from sklearn.datasets import make_blobs
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.ensemble import ExtraTreesClassifier
-from sklearn.tree import DecisionTreeClassifier
-
 from sklearn.preprocessing import OneHotEncoder, LabelEncoder
 
 homeDir = "/home/ray/"
@@ -24,8 +19,9 @@ trainingData = pd.read_csv(trainDataPath, dtype={"Pclass": "object"})
 # Remove columns I can't work with
 trainingData = trainingData.drop(["Name", "Ticket", "Cabin"], axis=1)
 
-
-#Set up one hot encoding
+###########################
+# Set up one hot encoding #
+###########################
 oneHotFeatures = ["Pclass", "Sex", "Embarked"]
 
 prepDf = trainingData[oneHotFeatures].copy() \
@@ -34,16 +30,18 @@ prepDf = trainingData[oneHotFeatures].copy() \
 lc = LabelEncoder()
 labelled = prepDf.apply(lc.fit_transform)
 
-
 ohe = OneHotEncoder()
 ohe.fit(labelled)
 
 oneHotTable = ohe.transform(labelled).toarray()
 
+###############################################
+# Join One Hot Columns back to starting table #
+###############################################
 
-# Join Columns back to starting table
 pdOneHot = pd.DataFrame(data=oneHotTable) \
              .reset_index()
+
 
 nonHotColumns = [x for x in trainingData.columns if x not in oneHotFeatures]
 nonHotTable = trainingData[nonHotColumns].copy() \
@@ -53,9 +51,18 @@ joinedTable = nonHotTable.join(pdOneHot, ["index"], lsuffix="_l", rsuffix="_r") 
                          .drop("index_r", axis=1) \
                          .drop("index_l", axis=1) \
                          .fillna(value=0)
+
+##############################################
+# Split data into training and test sections #
+##############################################
+
 joinedTable['is_train'] = np.random.uniform(0, 1, len(joinedTable)) <= .75
 
 trainData, testData = joinedTable[joinedTable['is_train']==True], joinedTable[joinedTable['is_train']==False]
+
+####################
+# Train Classifier #
+####################
 
 forestClassifier = RandomForestClassifier(n_estimators=10, max_depth=None, min_samples_split=2, random_state=0)
 
@@ -63,15 +70,14 @@ features = [x for x in joinedTable.columns if x not in ["Survived", "is_train"]]
 
 forestClassifier.fit(trainData[features], trainData["Survived"])
 
+###################
+# Test Classifier #
+###################
+
 result = forestClassifier.predict(testData[features])
-print(result)
 
 preds = pd.DataFrame(data=result)
 actual=testData["Survived"]
-
-print(len(result))
-print(len(testData))
-print(type(result))
 
 resultList = result.tolist()
 actualList = actual.tolist()
@@ -79,8 +85,11 @@ actualList = actual.tolist()
 combo = list(zip(resultList, actualList))
 review = [True if x[0] == x[1] else False for x in combo]
 
-print(review)
+correct = len([x for x in review if x == True])
+incorrect = len([x for x in review if x == False])
+
 
 print("Total: ", len(review))
-print("Correct: ", len([x for x in review if x == True]))
-print("Incorrect: ", len([x for x in review if x == False]))
+print("Correct: ", correct)
+print("Incorrect: ", incorrect)
+print("Accuracy: {0:.2f}%".format(correct/len(review) * 100))
